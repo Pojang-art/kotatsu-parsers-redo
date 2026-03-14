d99a1241e245093fb309c7ee6086336f7e73cc5a
Raphael Boullay <raphael.boullaylefur@lyceeconnecte.fr>
2026-03-14 23:19:01 +0100

    Update KunManga.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/KunManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/KunManga.kt
@@ -10,15 +10,15 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 internal class KunManga(context: MangaLoaderContext) :
     MadaraParser(context, MangaParserSource.KUNMANGA, "kunmanga.com", 10) {
 
-    override val headers: Headers = Headers.Builder()
-        .add(
+    override fun getRequestHeaders(): Headers = super.getRequestHeaders().newBuilder()
+        .set(
             "User-Agent",
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
                 "AppleWebKit/537.36 (KHTML, like Gecko) " +
                 "Chrome/120.0.0.0 Safari/537.36",
         )
-        .add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
-        .add("Accept-Language", "en-US,en;q=0.5")
-        .add("Referer", "https://kunmanga.com/")
+        .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+        .set("Accept-Language", "en-US,en;q=0.5")
+        .set("Referer", "https://kunmanga.com/")
         .build()
 }
