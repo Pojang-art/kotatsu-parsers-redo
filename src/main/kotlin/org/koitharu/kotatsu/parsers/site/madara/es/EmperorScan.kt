0fdefe883510c3d7ac9eb86e0000ce4690d17aba
Razan Muhammad Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-05-10 14:28:18 +0700

    Update EmperorScan.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/EmperorScan.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/es/EmperorScan.kt
@@ -7,4 +7,11 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("EMPERORSCAN", "EmperorScan", "es")
 internal class EmperorScan(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.EMPERORSCAN, "emperorscan.mundoalterno.org")
+	MadaraParser(context, MangaParserSource.EMPERORSCAN, "imperiomanhua.com") {
+
+	override val withoutAjax = true
+
+	override val datePattern = "MMMM dd, yyyy"
+
+	override val selectDesc = "div.summary_content div.post-content_item:has(h5:contains(Sinopsis)) div"
+}
