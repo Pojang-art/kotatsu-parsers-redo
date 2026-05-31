d404ba0547a13098ffac440d1fe9b36e0a85715f
Razan M. Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-05-31 18:13:16 +0700

    Update lumos.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/comicaso/id/lumos.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/comicaso/id/lumos.kt
@@ -3,11 +3,9 @@ package org.koitharu.kotatsu.parsers.site.comicaso.id
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
-import java.util.*
+import org.koitharu.kotatsu.parsers.site.comicaso.ComicasoParser
 
 @MangaSourceParser("LUMOSKOMIK", "LumosKomik", "id")
 internal class LumosKomik(context: MangaLoaderContext) :
     ComicasoParser(context, MangaParserSource.LUMOSKOMIK, "02.lumosgg.com") {
-    override val sourceLocale: Locale = Locale("id")
-    
 }
