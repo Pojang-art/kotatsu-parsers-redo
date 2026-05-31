245a28542fc4154a3ef02e005313a00c96582c69
Razan M. Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-05-31 18:05:26 +0700

    Update LumosKomik.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/id/LumosKomik.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/id/LumosKomik.kt
@@ -1,11 +1,13 @@
 package org.koitharu.kotatsu.parsers.site.madara.id
 
+import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 import java.util.*
 
+@Broken
 @MangaSourceParser("LUMOSKOMIK", "LumosKomik", "id")
 internal class LumosKomik(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.LUMOSKOMIK, "02.lumosgg.com") {
